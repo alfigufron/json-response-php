81f07ca3edc8760dca5ca8310f033d7d73399d82
M Alfi Gufron <56492746+alfigufron@users.noreply.github.com>
2021-08-14 20:41:23 +0700

    [CREATE] JSON Response PHP

---
+++ Response.php
@@ -0,0 +1,75 @@
+<?php
+
+class Response {
+  protected static $status;
+  protected static $http_code;
+  protected static $message;
+
+  public static function status($status) {
+    self::$status = $status;
+
+    return new self;
+  }
+
+  public static function code($code) {
+    self::$http_code = $code;
+
+    return new self;
+  }
+
+  public static function message($message) {
+    self::$message = $message;
+
+    return new self;
+  }
+
+  private function success($data = null) {
+    $code = self::$http_code;
+    $status = self::$status;
+    $message = self::$message;
+
+    $response = [
+      'meta' => [
+        'code' => ($code ? $code : 200),
+        'status' => $status,
+        'message' => ($message ? $message : null)
+      ]
+    ];
+
+    if ($data)
+      $response['data'] = $data;
+
+    return $response;
+  }
+
+  private function failed($error = null) {
+    $code = self::$http_code;
+    $status = self::$status;
+    $message = self::$message;
+
+    $response = [
+      'meta' => [
+        'code' => ($code ? $code : 400),
+        'status' => $status,
+        'message' => ($message ? $message : null)
+      ]
+    ];
+
+    if ($error)
+      $response['error'] = $error;
+
+    return $response;
+  }
+
+  public function result($data = null) {
+    $status = self::$status;
+    $code = self::$http_code;
+
+    if (empty($code))
+      $code = ($status === 'success')
+        ? 200
+        : 400;
+
+    return response()->json(self::$status($data), $code);
+  }
+}
